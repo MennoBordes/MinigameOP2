2963467465ede35e6d9a73a5e145ba1a589082e9
Menno Bordes <mennobordes@hetnet.nl>
2020-09-05 11:44:19 +0200

    Disable cx-freeze

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 # from cx_Freeze import setup, Executable
-import os.path
-from os import path
-PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-sprites_dir = path.join(path.dirname(__file__), 'sprites')
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
-build_exe_options = {"packages": ["os", "pygame", "turtle"]}
-options = {
-    'build_exe': {
-        'include_files': (
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-            sprites_dir,
-        ),
-        "packages": ["os", "pygame", "turtle"]
-    },
-}
+# import os.path
+# from os import path
+# PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+# sprites_dir = path.join(path.dirname(__file__), 'sprites')
+# os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+# os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+# build_exe_options = {"packages": ["os", "pygame", "turtle"]}
+# options = {
+#     'build_exe': {
+#         'include_files': (
+#             os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+#             os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+#             sprites_dir,
+#         ),
+#         "packages": ["os", "pygame", "turtle"]
+#     },
+# }
 
-setup(
-    name="Return to Eden",
-    version="0.1",
-    # options={"build_exe": build_exe_options},
-    # description="",
-    executables=[Executable("Intro.py")]
-
-    )
+# setup(
+#     name="Return to Eden",
+#     version="0.1",
+#     # options={"build_exe": build_exe_options},
+#     # description="",
+#     executables=[Executable("Intro.py")]
+#
+#     )
